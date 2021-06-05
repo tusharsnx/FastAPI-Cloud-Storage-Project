94bf35732e88472c5e70498011761755b2f74da0
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-05 20:13:00 +0530

    deleted django

--- main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from main.models import Products
-# Register your models here.
-
-admin.site.register(Products)
--- main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Products(models.Model):
-
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
