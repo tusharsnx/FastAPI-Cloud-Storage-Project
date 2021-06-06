5b5c08a0b01a71c4cab3d4f35f394611a938bfd2
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-06 21:10:40 +0530

    created post request on /blogs and Blogs model using BaseModel

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from typing  import Optional
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Blogs(BaseModel):
+    title: str = "10"
+    body: str = "body"
+    published_at: Optional[bool]
 
 @app.get("/blogs")
 @app.get('/')
 def blogs(limit: int = 100, sort: Optional[str] = None):
     return {"data" : limit}
+
+@app.post("/blogs")
+def create_blogs(blog: Blogs):
+    return blog.title
 
 @app.get("/blogs/unpublished")
 def blog_id(id: int):
