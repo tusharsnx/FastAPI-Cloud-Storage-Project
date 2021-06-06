a8d6c1ef9c1d6e5720746bc85a643253c7d5e86e
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-06 14:20:07 +0530

    added new routings in app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-
+from typing  import Optional
 app = FastAPI()
 
-@app.get("/index")
+@app.get("/blogs")
 @app.get('/')
-def index():
-    return {"data" :"tushrh: <h1>tushar</h1>"}
+def blogs(limit: int = 100, sort: Optional[str] = None):
+    return {"data" : limit}
+
+@app.get("/blogs/unpublished")
+def blog_id(id: int):
+    return {"id": id, "data": {"name": "Tushar Singh", "age": 19}}
+
+@app.get("/blogs/{id}")
+def blog_id(id: int):
+    return {"id": id, "data": {"name": "Tushar Singh", "age": 19}}
+
+@app.get("/blogs/{id}/age")
+def user_age(id: int):
+    return {"id": id, "age": 19}
