04d7943d5f8236950136b237afdb22783556dfe2
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-07 13:45:19 +0530

    added extra string validation at /search

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from typing  import Optional
+from fastapi import FastAPI, Query, Body
+from typing  import Optional, List
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,18 +12,25 @@ class Blogs(BaseModel):
 @app.get("/blogs")
 @app.get('/')
 def blogs(limit: int = 100, sort: Optional[str] = None):
-    return {"data" : limit}
+    return {"data" : limit, "sort": sort}
 
 @app.post("/blogs")
 def create_blogs(blog: Blogs):
     return blog.title
 
+
+@app.get("/search")
+# need to explicit define q as query param or else default to body param due to q not being in [str, int, bool, float](singular types)
+def search_blogs(q: List[str] = Query(..., deprecated=True, title="Tushar singh", description="Description")):
+    results = {"q":q}
+    return results
+
 @app.get("/blogs/unpublished")
-def blog_id(id: int):
+def blog_unpublished(id: int):
     return {"id": id, "data": {"name": "Tushar Singh", "age": 19}}
 
 @app.get("/blogs/{id}")
-def blog_id(id: int):
+def blog_users(id: int):
     return {"id": id, "data": {"name": "Tushar Singh", "age": 19}}
 
 @app.get("/blogs/{id}/age")
