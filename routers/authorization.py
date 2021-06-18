8b3e5806b505528b799029219595325aa72efc94
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-18 21:31:31 +0530

    tested login with google and successfully retrived, verified and decoded id_token

--- routers/authorization.py
+++ routers/authorization.py
@@ -1,11 +1,9 @@
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends,  Cookie
-from starlette.datastructures import URL
+from fastapi import APIRouter
 from starlette.responses import HTMLResponse
 from models import AuthUser
 from fastapi.requests import Request
 from fastapi.responses import Response, RedirectResponse
-from jose import jwt
+from jose import jws
 from uuid import uuid4
 import aiohttp
 import asyncio
@@ -16,69 +14,107 @@ CLIENT_ID = "722606380137-og8ok3tuotbrclko9fufih9ecdrb01a9.apps.googleuserconten
 CLIENT_SECRET = "C6nN5q32iylE7SFtH5ZFNp4g"
 REDIRECT_URI = "http://localhost:8000/auth/callback"
 SCOPE = "openid email"
-AUTH_Server = "https://accounts.google.com/o/oauth2/v2/auth?"
-Token_Server = "https://oauth2.googleapis.com/token?"
 
-google_query_params = f"client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope={SCOPE}&nonce={uuid4()}"
 
-auth = OAuth2PasswordBearer(tokenUrl="token")
+class OAuth2Handler:
+
+    def __init__(self, client_secret, client_id, redirect_url, scope):
+
+        self.client_secret = client_secret
+        self.client_id = client_id
+        self.token_endpoint = None
+        self.redirect_url = redirect_url
+        self.scope = scope
+        self.authorization_endpoint = None
+        self.jwks_uri = None
+
+    async def load_urls(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://accounts.google.com/.well-known/openid-configuration") as response:
+                response = await response.json()
+                self.jwks_uri = response.get("jwks_uri")
+                self.authorization_endpoint = response.get("authorization_endpoint")
+                self.token_endpoint = response.get("token_endpoint")
+
+        
+
+    def token_request_payload(self, code):
+        token_request_payload = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "redirect_uri": self.redirect_url
+        }
+        return token_request_payload
+
+    # authentication url with all required query parameter
+    def auth_server_url(self):
+        url = self.authorization_endpoint+"?"+f"client_id={self.client_id}&redirect_uri={self.redirect_url}&response_type=code&scope={self.scope}&nonce={uuid4()}"
+        return url
+
+    def encode_token(self):
+        pass
+
+    async def decode_token(self, token):
+        key = None
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.jwks_uri) as response:
+                key = await response.json()
+        data = jws.verify(token=token, key=key, algorithms="RS256")
+        return data
+
+    def authenticate(self):
+        pass
+
+    async def get_token_details(self, code):
+        token = None
+
+        # aiohttp for async access_token request
+        async with aiohttp.ClientSession() as session:
+            payload = self.token_request_payload(code)
+            async with session.post(self.token_endpoint, data=payload) as response:
+                token = await response.json()
+        return token
+
+
+auth = OAuth2Handler(
+    client_secret=CLIENT_SECRET, 
+    client_id=CLIENT_ID, 
+    redirect_url=REDIRECT_URI, 
+    scope=SCOPE
+    )
+
+loop = asyncio.get_event_loop()
+loop.create_task(auth.load_urls())
 router = APIRouter(prefix="/auth")
 
-# checks if user is logged in
-def get_current_user(request: Request, response: Response):
-    token = request.cookies.get("token")
-    if token:
-        # return jwt.decode(token, SECRET, algorithm = "HS256")
-        return AuthUser(username="username")
-    else:
-        print("redirecting.....")
-        return None
-        
-# returns username if user is logged in
-# @router.get("/profile")
-# def profile(user: AuthUser = Depends(get_current_user)):
-#     print("inside me")
-#     if not user:
-#         return RedirectResponse(url="http://localhost:8000/login")
-#     else:
-#         return {"user": user.username}
 
 # redirects to auth server if not logged in
 @router.get("/login", response_class=HTMLResponse)
-def login(response: Response):
-    # print("inside login")
-    # return RedirectResponse(url="http://localhost:8000/api/auth/callback?code=123")
-    return RedirectResponse(url=AUTH_Server+google_query_params)      # redirects to google auth server
+async def login(response: Response):
+    return RedirectResponse(url=auth.auth_server_url())      
 
-# callback url which receives code
+
+# callback url which receives code and create new user in the website
 @router.get("/callback")
 async def callback(request: Request, response: Response):
-    print("inside callback")
+    
+    # getting the code from query parameter
     code = request.query_params.get("code")
 
-    # aiohttp for async access_token request
-    payload = {
-        "grant_type": "authorization_code",
-        "code": code,
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "redirect_uri": REDIRECT_URI
-    }
-    token = None
-    async with aiohttp.ClientSession() as session:
-        async with session.post(Token_Server, data=payload) as response:
-            data = await response.json()
-            print(data)
-            token = data.get("id_token")
+    # exchanging code for token
+    token_details = await auth.get_token_details(code)
+    decoded_data = await auth.decode_token(token_details["id_token"])
 
-    response = RedirectResponse(url="http://localhost:8000/me")
-    response.set_cookie(key="token", value=token, httponly=True)
-    # return "done with cookie"
+    response = RedirectResponse(url="http://localhost:8000/profile")
+    response.set_cookie(key="id_token", value=token_details["id_token"], httponly=True)
     return response
 
+
 @router.get("/logout")
-def logout(response: Response):
-    response = RedirectResponse(url="http://localhost:8000/me")
+async def logout(response: Response):
+    response = RedirectResponse(url="http://localhost:8000/profile")
     response.delete_cookie("token")
     return response
 
--- routers/site.py
+++ routers/site.py
@@ -1,13 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
+from starlette.responses import RedirectResponse
+from models import AuthUser
 
 
 router = APIRouter()
 template = Jinja2Templates(directory="templates")
 
+def get_current_user(request: Request):
+    token = request.cookies.get("id_token")
+    if token:
+        # return token
+        # return jwt.decode(token, SECRET, algorithm = "HS256")
+        return AuthUser(username=token)
+    else:
+        return None
+
+
+@router.get("/profile")
+def profile(user: AuthUser = Depends(get_current_user)):
+    if user is None:
+        return RedirectResponse(url="http://localhost:8000/login")
+    else:
+        return {"username": user.username}
+
 @router.get("/login", response_class=HTMLResponse)
 async def index(request: Request):
     return template.TemplateResponse("test_login.html", {"request": request})
+
+@router.get("/logout")
+async def logout(request: Request):
+    return template.TemplateResponse("test_logout.html", {"request": request})
