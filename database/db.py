02cf2567cef08c21412f47806d393b970012880c
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-24 21:14:38 +0530

    created dashboard ui and added file list section

--- database/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///./db.db")
-
-dbsession = sessionmaker(bind=engine)
-
-Base = declarative_base()
