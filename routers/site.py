4f4b4eb8422f935410fa959dc3f06cb16d2286c4
Tushar Singh <tusharvickey1999@gmail.com>
2021-06-25 16:03:01 +0530

    added root route to serve homepage on it

--- routers/site.py
+++ routers/site.py
@@ -38,6 +38,7 @@ async def get_current_user(request: Request):
         return None
 
 
+@router.get("/")
 @router.get("/home", name="home")
 async def index(request: Request, user: User = Depends(get_current_user)):
     if user is None:
